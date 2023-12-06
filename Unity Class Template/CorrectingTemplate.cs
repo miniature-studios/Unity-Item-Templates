97b92f349e16f4529d385ac1aee814c5bd343fd5
Николай Химич <30412223+Kiuh@users.noreply.github.com>
2023-12-06 23:38:22 +0300

    Add project

---
+++ Unity Class Template/CorrectingTemplate.cs
@@ -0,0 +1,102 @@
+﻿using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Task = System.Threading.Tasks.Task;
+
+namespace Unity_Class_Template
+{
+    [ProvideAutoLoad(VSConstants.UICONTEXT.NoSolution_string, PackageAutoLoadFlags.BackgroundLoad)]
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [Guid(CorrectingTemplate.PackageGuidString)]
+    public sealed class CorrectingTemplate : AsyncPackage
+    {
+        private DTE2 _applicationObject;
+        private DocumentEvents _documentEvents;
+
+        public const string PackageGuidString = "1d77454c-bc85-4fc6-b63f-d97bedb09b8b";
+
+        public CorrectingTemplate() { }
+
+        protected override async Task InitializeAsync(
+            CancellationToken cancellationToken,
+            IProgress<ServiceProgressData> progress
+        )
+        {
+            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+
+            _applicationObject = await GetServiceAsync(typeof(DTE)) as DTE2;
+            _documentEvents = _applicationObject.Events.DocumentEvents;
+            _documentEvents.DocumentOpened += DocumentOpenedFirstTime;
+        }
+
+        private void DocumentOpenedFirstTime(Document Document)
+        {
+            JoinableTaskFactory.Run(
+                async delegate
+                {
+                    await JoinableTaskFactory.SwitchToMainThreadAsync();
+
+                    if (!(Document.Object("TextDocument") is TextDocument textDocument))
+                    {
+                        return;
+                    }
+
+                    if (textDocument.Language != "CSharp")
+                    {
+                        return;
+                    }
+
+                    if (File.GetCreationTime(Document.FullName) <= DateTime.Now.AddSeconds(-10))
+                    {
+                        return;
+                    }
+
+                    // Validation
+                    string[] toFind = new string[]
+                    {
+                        "MonoBehaviour",
+                        "AddComponentMenu",
+                        "internal class",
+                        "Awake"
+                    };
+
+                    foreach (string str in toFind)
+                    {
+                        if (!textDocument.StartPoint.CreateEditPoint().FindPattern(str))
+                        {
+                            return;
+                        }
+                    }
+
+                    // Replacements
+                    _ = textDocument.ReplaceText("Assets.Scripts.", string.Empty);
+                    _ = textDocument.ReplaceText(
+                        "[AddComponentMenu(\"",
+                        "[AddComponentMenu(\"Scripts/"
+                    );
+
+                    EditPoint start = textDocument.StartPoint.CreateEditPoint();
+                    if (start.FindPattern("[AddComponentMenu(\"Scripts/"))
+                    {
+                        string targetLine = start.GetLines(start.Line, start.Line + 1);
+                        string[] splits = targetLine.Split('/');
+                        for (int i = 0; i < splits.Length - 1; i++)
+                        {
+                            splits[i] = splits[i].Replace(".", "/");
+                        }
+                        string modifiedLine = splits.Aggregate((x, y) => x + "/" + y);
+                        _ = textDocument.ReplaceText(targetLine, modifiedLine);
+                    }
+
+                    _ = Document.Save();
+                }
+            );
+        }
+    }
+}
---
+++ Unity Class Template/Unity_Class_TemplatePackage.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Task = System.Threading.Tasks.Task;
+
+namespace Unity_Class_Template
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [Guid(Unity_Class_TemplatePackage.PackageGuidString)]
+    public sealed class Unity_Class_TemplatePackage : AsyncPackage
+    {
+        public const string PackageGuidString = "0e3062a1-46cb-4312-855d-10c347f6f953";
+
+        protected override async Task InitializeAsync(
+            CancellationToken cancellationToken,
+            IProgress<ServiceProgressData> progress
+        )
+        {
+            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+        }
+    }
+}
