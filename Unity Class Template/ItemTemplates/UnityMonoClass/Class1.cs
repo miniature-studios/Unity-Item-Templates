352aa79ecc4d7bb94b0fa12807e1bcd0ce1781a5
Николай Химич <30412223+Kiuh@users.noreply.github.com>
2024-01-18 20:11:29 +0300

    Cosmetic update

---
+++ Unity Class Template/ItemTemplates/UnityMonoClass/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+namespace $rootnamespace$
+{
+    [AddComponentMenu("$rootnamespace$/$rootnamespace$.$safeitemname$")]
+    internal class $safeitemname$ : MonoBehaviour 
+    {
+
+    }
+}
